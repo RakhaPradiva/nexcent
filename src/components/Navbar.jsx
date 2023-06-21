28f139fe7308314c0791bc025a0aed8e81c89a13
RakhaPradiva <rakhapradiva0803@gmail.com>
2023-06-21 09:42:46 +0700

    remove toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,42 +15,29 @@ const Navbar = () => {
 			<div className="w-[150px] h-[24px]">
 				<img src={logo} alt="logo" className="w-full h-full" />
 			</div>
+			<ul className="flex items-center justify-center gap-[50px]">
+				{navLinks.map((link) => (
+					<li key={link.id} className="font-normal text-[16px] text-center">
+						<a key={link.id} href={`#${link.id}`}>
+							{link.title}
+						</a>
+					</li>
+				))}
+			</ul>
 
-			<div className={`${toggle ? "flex" : "hidden"} max-sm:hidden`}>
-				<ul className="flex items-center justify-center gap-[50px]">
-					{navLinks.map((link) => (
-						<li key={link.id} className="font-normal text-[16px] text-center">
-							<a key={link.id} href={`#${link.id}`}>
-								{link.title}
-							</a>
-						</li>
-					))}
-				</ul>
-
-				<ul className="flex items-center justify-center gap-[10px]">
-					{navButtons.map((btn) => (
-						<li key={btn.id} className="font-normal text-[14px] text-center">
-							<a
-								key={btn.id}
-								href={`${btn.id}`}
-								className={`px-[20px] py-[10px] ${btn.style} rounded-[6px]`}
-							>
-								{btn.title}
-							</a>
-						</li>
-					))}
-				</ul>
-			</div>
-
-			<div className="max-sm:flex hidden items-center justify-center">
-				<button onClick={handleToggle}>
-					<img
-						src={toggle ? close : menu}
-						alt="menu"
-						className="w-[24px] h-[24px]"
-					/>
-				</button>
-			</div>
+			<ul className="flex items-center justify-center gap-[10px]">
+				{navButtons.map((btn) => (
+					<li key={btn.id} className="font-normal text-[14px] text-center">
+						<a
+							key={btn.id}
+							href={`${btn.id}`}
+							className={`px-[20px] py-[10px] ${btn.style} rounded-[6px]`}
+						>
+							{btn.title}
+						</a>
+					</li>
+				))}
+			</ul>
 		</nav>
 	);
 };
